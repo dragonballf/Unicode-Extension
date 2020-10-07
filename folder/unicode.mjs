524be62df8d2af253e86e764e9dc30712836b133
casto129 <70925177+casto129@users.noreply.github.com>
2020-10-07 14:33:37 +0900

    Create unicode.mjs

---
+++ folder/unicode.mjs
@@ -0,0 +1,53 @@
+/* Unicode Library | v1.0.0 
+CopyRight Jay 2020(c)
+*/
+'use strict';
+  
+// Special Unicode
+var U3164 = 'ㅤ';
+
+// Number Unicode
+var U2460 = '①';
+var U2461 = '②';
+var U2462 = '③';
+var U2463 = '④';
+var U2464 = '⑤';
+var U2465 = '⑥';
+var U2466 = '⑦';
+var U2467 = '⑧';
+var U2468 = '⑨';
+var U2469 = '⑩';
+
+// Alphabet Unicode
+var U24B6 = 'Ⓐ';
+var U24B7 = 'Ⓑ';
+var U24B8 = 'Ⓒ';
+var U24B9 = 'Ⓓ';
+var U24BA = 'Ⓔ';
+var U24BB = 'Ⓕ';
+var U24BC = 'Ⓖ';
+var U24BD = 'Ⓗ';
+var U24BE = 'Ⓘ';
+var U24BF = 'Ⓙ';
+var U24C0 = 'Ⓚ';
+var U24C1 = 'Ⓛ';
+var U24C2 = 'Ⓜ';
+var U24C3 = 'Ⓝ';
+var U24C4 = 'Ⓞ';
+var U24C5 = 'Ⓟ';
+var U24C6 = 'Ⓠ';
+var U24C7 = 'Ⓡ';
+var U24C8 = 'Ⓢ';
+var U24C9 = 'Ⓣ';
+var U24CA = 'Ⓤ';
+var U24CB = 'Ⓥ';
+var U24CC = 'Ⓦ';
+var U24CD = 'Ⓧ';
+var U24CE = 'Ⓨ';
+var U24CF = 'Ⓩ';
+
+export {U3164, U2460, U2461, U2462, U2463, U2464, U2465, U2466, U2467, 
+        U2468, U2469, U24B6, U24B7, U24B8, U24B9, U24BA, U24BB, U24BC,
+        U24BD, U24BE, U24BF, U24C0, U24C1, U24C2, U24C3, U24C4, U24C5,
+        U24C6, U24C7, U24C8, U24C9, U24CA, U24CB, U24CC, U24CD, U24CE,
+        U24CF};
