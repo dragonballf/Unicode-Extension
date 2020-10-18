c6f02a7602379bc2a6bb34b117334f872b28988d
casto129 <70925177+casto129@users.noreply.github.com>
2020-10-18 22:54:10 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,89 @@
+/* Unicode Library | v1.0.1
+CopyRight ento32940 2020(c)
+*/
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.U24CF = exports.U24CE = exports.U24CD = exports.U24CC = exports.U24CB = exports.U24CA = exports.U24C9 = exports.U24C8 = exports.U24C7 = exports.U24C6 = exports.U24C5 = exports.U24C4 = exports.U24C3 = exports.U24C2 = exports.U24C1 = exports.U24C0 = exports.U24BF = exports.U24BE = exports.U24BD = exports.U24BC = exports.U24BB = exports.U24BA = exports.U24B9 = exports.U24B8 = exports.U24B7 = exports.U24B6 = exports.U2469 = exports.U2468 = exports.U2467 = exports.U2466 = exports.U2465 = exports.U2464 = exports.U2463 = exports.U2462 = exports.U2461 = exports.U2460 = exports.U3164 = void 0;
+
+ // Special Unicode
+var U3164 = 'ㅤ'; 
+exports.U3164 = U3164;
+
+// Number Unicode
+var U2460 = '①';
+exports.U2460 = U2460;
+var U2461 = '②';
+exports.U2461 = U2461;
+var U2462 = '③';
+exports.U2462 = U2462;
+var U2463 = '④';
+exports.U2463 = U2463;
+var U2464 = '⑤';
+exports.U2464 = U2464;
+var U2465 = '⑥';
+exports.U2465 = U2465;
+var U2466 = '⑦';
+exports.U2466 = U2466;
+var U2467 = '⑧';
+exports.U2467 = U2467;
+var U2468 = '⑨';
+exports.U2468 = U2468;
+var U2469 = '⑩'; 
+exports.U2469 = U2469;
+
+// Alphabet Unicode
+var U24B6 = 'Ⓐ';
+exports.U24B6 = U24B6;
+var U24B7 = 'Ⓑ';
+exports.U24B7 = U24B7;
+var U24B8 = 'Ⓒ';
+exports.U24B8 = U24B8;
+var U24B9 = 'Ⓓ';
+exports.U24B9 = U24B9;
+var U24BA = 'Ⓔ';
+exports.U24BA = U24BA;
+var U24BB = 'Ⓕ';
+exports.U24BB = U24BB;
+var U24BC = 'Ⓖ';
+exports.U24BC = U24BC;
+var U24BD = 'Ⓗ';
+exports.U24BD = U24BD;
+var U24BE = 'Ⓘ';
+exports.U24BE = U24BE;
+var U24BF = 'Ⓙ';
+exports.U24BF = U24BF;
+var U24C0 = 'Ⓚ';
+exports.U24C0 = U24C0;
+var U24C1 = 'Ⓛ';
+exports.U24C1 = U24C1;
+var U24C2 = 'Ⓜ';
+exports.U24C2 = U24C2;
+var U24C3 = 'Ⓝ';
+exports.U24C3 = U24C3;
+var U24C4 = 'Ⓞ';
+exports.U24C4 = U24C4;
+var U24C5 = 'Ⓟ';
+exports.U24C5 = U24C5;
+var U24C6 = 'Ⓠ';
+exports.U24C6 = U24C6;
+var U24C7 = 'Ⓡ';
+exports.U24C7 = U24C7;
+var U24C8 = 'Ⓢ';
+exports.U24C8 = U24C8;
+var U24C9 = 'Ⓣ';
+exports.U24C9 = U24C9;
+var U24CA = 'Ⓤ';
+exports.U24CA = U24CA;
+var U24CB = 'Ⓥ';
+exports.U24CB = U24CB;
+var U24CC = 'Ⓦ';
+exports.U24CC = U24CC;
+var U24CD = 'Ⓧ';
+exports.U24CD = U24CD;
+var U24CE = 'Ⓨ';
+exports.U24CE = U24CE;
+var U24CF = 'Ⓩ';
+exports.U24CF = U24CF;
